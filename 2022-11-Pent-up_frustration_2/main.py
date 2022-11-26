12881b04561ac22f65334c7ce75a3e77e540fbaf
zorzigio <zorzi.gio@gmail.com>
2022-11-26 20:54:08 +0400

    Pentagons class

--- 2022-11-Pent-up_frustration_2/main.py
+++ 2022-11-Pent-up_frustration_2/main.py
@@ -1,20 +1,51 @@
-from shapely.geometry import Point
-import matplotlib.pyplot as plt
-from pentagon import Pentagon
+from pentagon import Pentagons
+from itertools import product
+from datetime import datetime
+x = [2, 3, 4, 5]
+
+
+problematic_sequences = [
+    [2, 3, 3, 2]
+]
+
+
+def contains(sequence: list[int] | tuple[int], sub_sequence: list[list[int]]) -> bool:
+    def to_string(sequence: list[int] | tuple[int]) -> str:
+        return "".join(map(str, sequence))
+    s = to_string(sequence)
+    for sub in sub_sequence:
+        if to_string(sub) in s:
+            return True
+    return False
+
 
 if __name__ == "__main__":
-    p1 = Point(0, 0)
-    p2 = Point(0, 1)
-    P1 = Pentagon(p1, p2)
-    P2 = Pentagon(p2, p1)
+    # pents = Pentagons.from_sequence([3, 2])
+    # pents.plot()
+    # pents = Pentagons.from_sequence([2, 2, 2, 2, 4])
+    # pents.plot()
 
-    # plot
-    fig = plt.figure(1, figsize=(5, 5), dpi=90)
-    ax = fig.add_subplot(111)
-    P1.plot(ax)
-    P2.plot(ax)
-    extent = 10
-    ax.set_xlim(-extent, extent)
-    ax.set_ylim(-extent, extent)
-    ax.set_aspect(1)
-    plt.show()
+    # open file with datetime stamp
+    with open(f"pentagons_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt", "w") as f:
+
+        min_distance = float("inf")
+        sequence = []
+        for i, perm in enumerate(product(x, repeat=15)):
+            if i % 1000 == 0:
+                print(i)
+            if contains(perm, problematic_sequences):
+                continue
+            pents = Pentagons.from_sequence(list(perm))
+            if pents.has_collisions:
+                continue
+            if pents.distance < min_distance:
+                min_distance = pents.distance
+                sequence = pents.sequence
+                f.write(f"{min_distance:.7f} {sequence}")
+                print(f"New min distance: {min_distance:.7f}")
+                print(f"Sequence: {sequence}")
+
+            # pents.plot()
+        # pents.plot()
+    print(f"New min distance: {min_distance:.7f}")
+    print(f"Sequence: {sequence}")
--- 2022-11-Pent-up_frustration_2/pentagon.py
+++ 2022-11-Pent-up_frustration_2/pentagon.py
@@ -5,16 +5,24 @@ from descartes import PolygonPatch
 import math
 
 
+class LineStringPlus(LineString):
+    @property
+    def points(self) -> tuple[Point, Point]:
+        return (Point(self.coords[0]), Point(self.coords[1]))
+
+
 class Pentagon:
     SIDE_LENGTH = 1
     ANGLE_INTERNAL = 2 * math.pi - math.pi * 2 / 5
     R = (SIDE_LENGTH / 2) / math.sin(math.pi / 5)
     r = (SIDE_LENGTH / 2) / math.tan(math.pi / 5)
+    COLLISION_DISTANCE = 1e-6
 
     def __init__(
         self,
         p1: Point,
         p2: Point,
+        label: str | None = None,
     ) -> None:
         """
         Create a pentagon with side length 1 given one side defined by p1 and p2.
@@ -22,10 +30,7 @@ class Pentagon:
         """
         self.p1 = p1
         self.p2 = p2
-        # Calculate the side length
-        side_length = p1.distance(p2)
-        if side_length != self.SIDE_LENGTH:
-            raise ValueError("Side length is not 1")
+        self.label = label
 
         # Calculate the angle between p1 and p2
         angle = math.atan2(p2.y - p1.y, p2.x - p1.x)
@@ -46,11 +51,14 @@ class Pentagon:
         self.poly = Polygon([self.p1, self.p2, self.p3, self.p4, self.p5])
         self.p: tuple[Point, Point, Point, Point, Point] = (self.p1, self.p2, self.p3, self.p4, self.p5)
 
-    def side(self, n: int) -> LineString:
-        return LineString([self.p[n-1], self.p[(n)]])
+    def side(self, n: int) -> LineStringPlus:
+        return LineStringPlus([self.p[n-1], self.p[(n)]])
 
-    def R_line(self, n: int) -> LineString:
-        return LineString([self.center_point, self.p[(n-1)]])
+    def R_line(self, n: int) -> LineStringPlus:
+        return LineStringPlus([self.center_point, self.p[(n-1)]])
+
+    def get_pentagon_on_side(self, n: int, label: str | None = None) -> "Pentagon":
+        return Pentagon(self.p[n % 5], self.p[n-1], label=label)
 
     def plot(self, ax: plt.Axes) -> None:
         patch = PolygonPatch(self.poly, fc='#999999', ec='#000000', alpha=0.5, zorder=2)
@@ -58,3 +66,83 @@ class Pentagon:
         plt.plot(*self.side(1).xy)
         plt.plot(*self.R_line(1).xy)
         plt.plot(self.center_point.x, self.center_point.y, 'ro')
+        if self.label is not None:
+            plt.text(self.center_point.x, self.center_point.y, self.label)
+
+
+class Pentagons:
+
+    def __init__(self) -> None:
+        self.pentagons: list[Pentagon] = []
+        self.distance: float = float("inf")
+        self.has_collisions: bool = False
+        "Starts with 2 pentagons"
+        p1 = Point(0, 1)
+        p2 = Point(0, 0)
+        P1 = Pentagon(p1, p2, label="P1")
+        self.pentagons.append(P1)
+        P2 = P1.get_pentagon_on_side(1, label="P2")
+        self.pentagons.append(P2)
+        self.sequence: list[int] = []
+
+    @ classmethod
+    def from_sequence(cls, sequence: list[int]) -> "Pentagons":
+        pents = cls()
+        for n in sequence:
+            pents.add(n)
+            if pents.has_collisions:
+                break
+        return pents
+
+    def add(self, n: int) -> None:
+        P = self.pentagons[-1].get_pentagon_on_side(n, label=f"P{len(self)+1}")
+        # print(f"Adding pentagon {P.label} on side {n}")
+        self.pentagons.append(P)
+        self.sequence.append(n)
+        self.has_collisions = self.check_for_collisions()
+        if not self.has_collisions:
+            self.distance = self._compute_min_distance()
+
+    def plot(self) -> None:
+        fig = plt.figure(1, figsize=(5, 5), dpi=90)
+        ax = fig.add_subplot(111)
+        for P in self:
+            P.plot(ax)
+        extent = 10
+        ax.set_xlim(-extent, extent)
+        ax.set_ylim(-extent, extent)
+        ax.set_aspect(1)
+        plt.title(f"Distance: {self.distance:.7f}")
+        plt.show()
+
+    def __len__(self) -> int:
+        return len(self.pentagons)
+
+    def __getitem__(self, i: int) -> Pentagon:
+        return self.pentagons[i]
+
+    def _compute_min_distance(self) -> float:
+        if len(self) <= 3:
+            return float("inf")
+        last_pentagon = self[-1]
+        min_distance = self.distance
+        for pentagon in self.pentagons[0:-3]:
+            distance = pentagon.poly.distance(last_pentagon.poly)
+            if distance < min_distance:
+                min_distance = distance
+        if min_distance < Pentagon.COLLISION_DISTANCE:
+            self.has_collisions = True
+        return min_distance
+
+    def check_for_collisions(self) -> bool:
+        if self.has_collisions:
+            return True
+        if len(self) <= 2:
+            return False
+        last_pentagon = self[-1]
+        for pentagon in self.pentagons[:-2]:
+            intersection = pentagon.poly.intersection(last_pentagon.poly)
+            if intersection.area > 0:
+                return True
+
+        return False
